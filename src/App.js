f4d04f3b1f4aaf98e7fdde70780cbf0cc1809fc5
Kelvin Anderson <kelvinanders17@gmail.com>
2022-08-27 13:16:14 +0700

    [DONE]

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import guidelines from './guidelines.xlsx'
+import template from './template.xlsx'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button>
+      <a href={guidelines} download="Upload Guidelines.xlsx">Download Guidelines</a>
+      </button>
+      <button>
+      <a href={template} download="Template.xlsx">Download Template</a>
+      </button>
     </div>
   );
 }
